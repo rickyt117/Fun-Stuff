205cfeef1af78a3cf369c14c18df1f31587053c3
unknown <rickyt117@bitbucket.com>
2015-02-20 21:40:48 -0800

    Added Computer AI Found Major Win error (edge case)

--- SOS/board.rb
+++ SOS/board.rb
@@ -1,10 +1,11 @@
 class Board
     BOARD_CLEANER = 100
     BOARD_MAX_INDEX = 9
+    BOARD_MAX_SQ = (BOARD_MAX_INDEX + 1) ** 2
     EMPTY_POS = ' '
     @@counter = 0
-    @@recent_letter = ' '
     @@recent_play = 0
+    @@recent_letter = ' '
     
     def initialize(current_player)
         @current_player = current_player
@@ -37,35 +38,41 @@ class Board
     end
     
     def board_full
-        if @@counter == (BOARD_MAX_INDEX + 1) ** 2
+        if @@counter == BOARD_MAX_SQ
             return true
         else 
             return false
         end
     end
     
-    def ask_player_for_move(current_player)
-        played = false
-        while not played
-            puts "Player " + current_player + ": What square would you like to play?"
-            @@recent_play = gets.to_i - BOARD_CLEANER
-            puts "Player " + current_player + ": What would you like to play?"
-            @@recent_letter = gets.chomp
-            column = @@recent_play % (BOARD_MAX_INDEX + 1)
-            row = @@recent_play / (BOARD_MAX_INDEX + 1)
-            if validate_position(@@recent_play, row, column, @@recent_letter)
+    def ask_player_for_move(current_player)            
+        while 1 < 2
+            if current_player != "BOT"
+                puts "Player " + current_player + ": What square would you like to play?"
+                @@recent_play = gets.to_i - BOARD_CLEANER
+                puts "Player " + current_player + ": What would you like to play?"
+                @@recent_letter = gets.chomp
+            else 
+                @@recent_play, @@recent_letter = computer_move
+            end
+            
+            if validate_position(@@recent_play, @@recent_letter)
+                column = @@recent_play % (BOARD_MAX_INDEX + 1)
+                row = @@recent_play / (BOARD_MAX_INDEX + 1)
                 @board[row][column] = @@recent_letter
-                played = true
                 @@counter += 1
+                return check_win(@@recent_play, @@recent_letter)
             end
         end
     end
     
-    def validate_position(play, row, column, letter)
+    def validate_position(play, letter)
         if play >= (BOARD_MAX_INDEX + 1) ** 2 or play < 0
             puts "Position invalid, please try again"
             return false
         end
+        column = play % (BOARD_MAX_INDEX + 1)
+        row = play / (BOARD_MAX_INDEX + 1)
         if row <= BOARD_MAX_INDEX and column <= BOARD_MAX_INDEX
             if @board[row][column] == EMPTY_POS
                 if letter == 'S' or letter == 'O'
@@ -100,21 +107,21 @@ class Board
         end
     end
     
-    def check_win
-        temp_recent = @@recent_play
+    def check_win(play, letter)
+        temp_play = play
         win = 0
-        if @@recent_letter == 'O'
+        if letter == 'O'
             #Check horizontal and one vertical
             #  x
             #xxx
             #x 
-            if check_square(temp_recent + 1) == 'S'               
-                if check_square(temp_recent - 1) == 'S'
+            if check_square(temp_play + 1) == 'S'               
+                if check_square(temp_play - 1) == 'S'
                     win += 1
                 end
             end
-            if check_square(temp_recent - BOARD_MAX_INDEX) == 'S'
-                if check_square(temp_recent + BOARD_MAX_INDEX) == 'S'
+            if check_square(temp_play - BOARD_MAX_INDEX) == 'S'
+                if check_square(temp_play + BOARD_MAX_INDEX) == 'S'
                     win += 1
                 end
             end
@@ -123,65 +130,105 @@ class Board
             #xx
             # x
             # xx
-            if check_square(temp_recent + BOARD_MAX_INDEX + 1) == 'S'
-                if check_square(temp_recent - BOARD_MAX_INDEX - 1) == 'S'
+            if check_square(temp_play + BOARD_MAX_INDEX + 1) == 'S'
+                if check_square(temp_play - BOARD_MAX_INDEX - 1) == 'S'
                     win += 1
                 end
             end
-            if check_square(temp_recent + BOARD_MAX_INDEX + 2) == 'S'
-                if check_square(temp_recent - BOARD_MAX_INDEX - 2) == 'S'
+            if check_square(temp_play + BOARD_MAX_INDEX + 2) == 'S'
+                if check_square(temp_play - BOARD_MAX_INDEX - 2) == 'S'
                     win += 1
                 end
             end
             
-        elsif @@recent_letter == 'S'
+        elsif letter == 'S'
             #Check horizontal
-            if check_square(temp_recent + 1) == 'O'
-                if check_square(temp_recent + 2) == 'S'
+            if check_square(temp_play + 1) == 'O'
+                if check_square(temp_play + 2) == 'S'
                     win += 1
                 end
             end  
-            if check_square(temp_recent - 1) == 'O'
-                if check_square(temp_recent - 2) == 'S'
+            if check_square(temp_play - 1) == 'O'
+                if check_square(temp_play - 2) == 'S'
                     win += 1
                 end
             end
             
             #Check vertical
-            if check_square(temp_recent + BOARD_MAX_INDEX + 1) == 'O'
-                if check_square(temp_recent + 2 * BOARD_MAX_INDEX + 2) == 'S'
+            if check_square(temp_play + BOARD_MAX_INDEX + 1) == 'O'
+                if check_square(temp_play + 2 * BOARD_MAX_INDEX + 2) == 'S'
                     win += 1
                 end
             end            
-            if check_square(temp_recent - BOARD_MAX_INDEX - 1) == 'O'
-                if check_square(temp_recent - 2 * BOARD_MAX_INDEX - 2) == 'S'
+            if check_square(temp_play - BOARD_MAX_INDEX - 1) == 'O'
+                if check_square(temp_play - 2 * BOARD_MAX_INDEX - 2) == 'S'
                     win += 1
                 end
             end
             
             #Check diagonal
-            if check_square(temp_recent - BOARD_MAX_INDEX) == 'O'
-                if check_square(temp_recent - 2 * BOARD_MAX_INDEX) == 'S'
+            if check_square(temp_play - BOARD_MAX_INDEX) == 'O'
+                if check_square(temp_play - 2 * BOARD_MAX_INDEX) == 'S'
                     win += 1
                 end
             end
-            if check_square(temp_recent + BOARD_MAX_INDEX) == 'O'
-                if check_square(temp_recent + 2 * BOARD_MAX_INDEX) == 'S'
+            if check_square(temp_play + BOARD_MAX_INDEX) == 'O'
+                if check_square(temp_play + 2 * BOARD_MAX_INDEX) == 'S'
                     win += 1
                 end
             end
             
-            if check_square(temp_recent - BOARD_MAX_INDEX - 2) == 'O'
-                if check_square(temp_recent - 2 * (BOARD_MAX_INDEX + 2)) == 'S'
+            if check_square(temp_play - BOARD_MAX_INDEX - 2) == 'O'
+                if check_square(temp_play - 2 * (BOARD_MAX_INDEX + 2)) == 'S'
                     win += 1
                 end
             end
-            if check_square(temp_recent + BOARD_MAX_INDEX + 2) == 'O'
-                if check_square(temp_recent + 2 * (BOARD_MAX_INDEX + 2)) == 'S'
+            if check_square(temp_play + BOARD_MAX_INDEX + 2) == 'O'
+                if check_square(temp_play + 2 * (BOARD_MAX_INDEX + 2)) == 'S'
                     win += 1
                 end
             end
         end
         return win
     end
+    
+    def computer_move
+        max_score = 0
+        preferred_move = -1
+        letter = ' '
+        for i in 0..BOARD_MAX_SQ
+            play = i
+            if check_square(i) != ' ' 
+                next
+            end
+            if self.check_win(i, 'S') > max_score
+                max_score = self.check_win(i, 'S')
+                preferred_move = i
+                letter = 'S'
+            end
+            if self.check_win(i, 'O') > max_score
+                max_score = self.check_win(i, 'O')
+                preferred_move = i
+                letter = 'O'
+            end
+        end
+        if max_score == 0
+            while 1 < 2
+                preffered_move = rand(BOARD_MAX_SQ)
+                column = play % (BOARD_MAX_INDEX + 1)
+                row = play / (BOARD_MAX_INDEX + 1)
+                if rand(3) >= 1
+                    letter = 'S'
+                else
+                    letter = 'O'
+                end
+                if validate_position(preffered_move, letter)
+                    return preffered_move, letter
+                end
+            end     
+        else
+            return preferred_move, letter
+        end
+    end
+    
 end
--- SOS/sos.rb
+++ SOS/sos.rb
@@ -1,30 +1,41 @@
 require_relative 'board.rb'
 
-puts "Starting SOS"
-
 players = ['R', 'B']
 r_score = 0;
 b_score = 0;
 
-current_player = players[rand(2)]
-b = Board.new(current_player)
+puts "Starting SOS"
+
+puts "PVP or PVE?"
+
+game = gets.chomp
+
+current_player = 'B'
+b = Board.new('B')
 b.display()
 puts
 
 while not b.board_full()
-    b.ask_player_for_move(current_player)
-    current_player = b.get_next_turn()
     if current_player == 'B'
-        r_score += b.check_win()
-    else 
-        b_score += b.check_win()
+        b_score += b.ask_player_for_move(current_player)
     end
+    
+    if current_player == 'R'
+        if game == "PVP"
+            r_score += b.ask_player_for_move(current_player)
+        elsif game == "PVE" 
+            r_score += b.ask_player_for_move("BOT")
+        end
+    end
+    current_player = b.get_next_turn()
     b.display()
     puts "R SCORE: " + r_score.to_s + " B SCORE: " + b_score.to_s
 end
 
 if r_score > b_score
     puts "R WINS"
-else 
+elsif b_score > r_score
     puts "B WINS"
+else
+    puts "TIE"
 end
